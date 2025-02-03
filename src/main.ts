e82aaa555d1bd871970f33ba2955d59c65644555
Saman <samanmahmoudian.sm@gmail.com>
2025-02-03 18:13:31 +0330

    .

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 app.enableCors()
 
-  const peerServer = PeerServer({host:'localhost'});
+  const peerServer = PeerServer({host:'localhost',port:443, path:'/'});
   await app.listen(3000);
 }
 bootstrap();
