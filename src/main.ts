bad0e5c5c63f4af23c82e6cb52c717cd072b65cb
Saman <samanmahmoudian.sm@gmail.com>
2025-02-03 18:52:19 +0330

    .

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import { PeerServer } from 'peer';
-const cors = require('cors')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-app.enableCors()
-
+  app.enableCors({
+    origin:'https://front-n04k.onrender.com'
+  })
   const peerServer = PeerServer({host:'localhost',port:443, path:'/'});
   await app.listen(3000);
 }
--- src/socket/socket.module.ts
+++ src/socket/socket.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SocketGateway } from './socket.gateway';
-import { SocketController } from './socket.controller';
+
 
 @Module({
   providers: [SocketGateway],
-  controllers: [SocketController]
 })
 export class SocketModule {}
