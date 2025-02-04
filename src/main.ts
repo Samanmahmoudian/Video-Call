4246bf8d14a8be888166aff0310fddcb191f15a0
Saman <samanmahmoudian.sm@gmail.com>
2025-02-04 15:23:12 +0330

    .

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { PeerServer , ExpressPeerServer } from 'peer';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({origin:'https://front-n04k.onrender.com'})
 const server = await app.getHttpServer()
   const peerServer = ExpressPeerServer(server);
   app.listen(3000)
