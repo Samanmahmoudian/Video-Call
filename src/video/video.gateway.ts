e8b575e4ee2d7c3e8bfa8945d3179d61f80d7216
Saman <samanmahmoudian.sm@gmail.com>
2025-02-04 16:54:30 +0330

    .

--- src/video/video.gateway.ts
+++ src/video/video.gateway.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({cors:{origin:'https://front-n04k.onrender.com'}})
 export class VideoGateway {
   @WebSocketServer()
   server: Server;
