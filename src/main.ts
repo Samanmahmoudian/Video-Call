723a214ede4dbb07a7480fcfa2e561b7edd2f7cd
Saman <samanmahmoudian.sm@gmail.com>
2025-02-04 13:41:36 +0330

    .

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
 
-const peerServer = PeerServer({host:'127.0.0.1' , path:'/peerjs'})
+const peerServer = PeerServer({host:'https://video-call-uylq.onrender.com'})
   await app.listen(3000);
 }
 
