0851be3509fb4ac31b4daa564a44c93d6acc9e3f
Saman <samanmahmoudian.sm@gmail.com>
2025-02-04 15:18:15 +0330

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { PeerService } from './peer/peer.service';
 
 @Module({
-  providers: [PeerService],
+
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PeerServer , ExpressPeerServer } from 'peer';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+const server = await app.getHttpServer()
+  const peerServer = ExpressPeerServer(server);
+  app.listen(3000)
 }
 
 bootstrap();
